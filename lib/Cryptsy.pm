61b69358645c7f28166912f8b2b9ad024b82ef77
Glen Hinkle <glen@empireenterprises.com>
2013-12-25 21:47:53 -0500

    market data

--- lib/Cryptsy.pm
+++ lib/Cryptsy.pm
@@ -1,5 +1,46 @@
-use strict;
-use warnings;
 package Cryptsy;
+use feature 'say';
+use Mojo::Base -base;
+use Mojo::UserAgent;
+
+has ua => sub { Mojo::UserAgent->new };
+
+sub _headers { {} }
+
+sub all_markets {
+  my $self = shift;
+  my $ua   = $self->ua;
+  my $cb   = shift;
+
+  $ua->get(
+    'http://pubapi.cryptsy.com/api.php?method=marketdatav2' =>
+      $self->_headers() => sub {
+      my ($ua, $tx) = @_;
+      $tx->res->json('/success')
+        ? $cb->($tx->res->json('/return/markets'))
+        : $cb->();
+    }
+  );
+}
+
+sub market {
+  my $self = shift;
+  my $ua   = $self->ua;
+  my $id   = shift;
+  my $cb   = shift;
+
+  $ua->get(
+    Mojo::URL->new('http://pubapi.cryptsy.com/api.php')
+      ->query(method => 'singlemarketdata', marketid => $id) =>
+      $self->_headers() => sub {
+      my ($ua, $tx) = @_;
+
+      return $cb->() if not $tx->res->json('/success');
+
+      my $json = $tx->res->json('/return/markets');
+      $cb->($json->{shift [keys $json]});
+    }
+  );
+}
 
 1;
